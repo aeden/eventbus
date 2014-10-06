98547d34a940449ec097b03a6fe1ad7537a857ef
Anthony Eden <anthonyeden@gmail.com>
2014-10-06 17:22:06 +0200

    Working on registration flow.

--- static/index.js
+++ static/index.js
@@ -7,33 +7,26 @@ $(document).ready(function() {
 
   // This is an app-specific event listener. It's job is to update the UI
   EventBus.listen(App, 'check.domain', function(evt, domainName) {
-    $('#new-domain').hide();
+    $('#new-domain').addClass("hidden");
 
     $('#checking-domain .domain-name').html(domainName);
-    $('#checking-domain').show();
+    $('#checking-domain').removeClass("hidden");
   });
 
   // Currently this can only handle one result
   EventBus.listen(App, 'check.domain.result', function(evt, result) {
-    $('#checking-domain').hide();
+    $('#checking-domain').addClass("hidden");
 
     $('#domain-check-result .domain-name').html(result.name);
     $('#domain-check-result .domain-availability').html(result.availability);
 
     if (result.availability === 'available') {
       $(".check-result-icon").addClass('glyphicon').addClass('glyphicon-ok');
-      $('#domain-registration').show();
-
-      $('#search-again-link').on("click", function(evt) {
-        $('#domain-name').val("");
-        $('#new-domain').show();
-
-        $('#domain-registration').hide();
-        $('#domain-check-result').hide();
-      });
+      $(".check-result").addClass('alert-success');
+      $('#domain-registration').removeClass("hidden");
     }
 
-    $('#domain-check-result').show();
+    $('#domain-check-result').removeClass("hidden");
   });
 
 
@@ -41,6 +34,19 @@ $(document).ready(function() {
   $('#new-domain').on("submit", function(evt) {
     evt.preventDefault();
     EventBus.send(App, 'check.domain', [$("#domain-name").val()]);
-  }); 
+  });
+
+  $('#register-domain').on("submit", function(evt) {
+    evt.preventDefault();
+    EventBus.send(App, 'register.domain', $('#register-domain').serializeJSON());
+  });
+
+  $('#search-again-link').on("click", function(evt) {
+    $('#domain-name').val("");
+    $('#new-domain').removeClass("hidden");
+
+    $('#domain-registration').addClass("hidden");
+    $('#domain-check-result').addClass("hidden");
+  });
 
 });
