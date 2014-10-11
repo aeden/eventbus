2fff4b6ac27a42f912463cd595f9e06082cac76c
Anthony Eden <anthonyeden@gmail.com>
2014-10-12 00:18:53 +0200

    Create an authenticator interface and a default authenticator.

--- services.go
+++ services.go
@@ -3,3 +3,13 @@ package eventbus
 type ServicesConfig struct {
 	Services []map[string]string
 }
+
+type Authenticator interface {
+	Authenticate(credentials interface{}) (bool, error)
+}
+
+type DefaultAuthenticator struct{}
+
+func (authenticator *DefaultAuthenticator) Authenticate(credentials interface{}) (bool, error) {
+	return true, nil
+}
