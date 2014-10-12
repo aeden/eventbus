954149635edd083849d3ecd25e0b020471479951
Anthony Eden <anthonyeden@gmail.com>
2014-10-12 17:37:21 +0200

    Start a file for package documentation.

---
+++ eventbus.go
@@ -0,0 +1,6 @@
+// Copyright 2014, Anthony Eden 
+
+/*
+Package eventstore provides a remote event bus that accepts new events via HTTP POSTs and broadcasts events via WebSockets.
+*/
+package eventbus
