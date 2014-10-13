5ea77a27328a911fe802978f05b19ce1b6a82f66
Anthony Eden <anthonyeden@gmail.com>
2014-10-13 13:04:09 +0200

    Move routing into the server for now.
    Eventually this may be broken out into its own, but
    for the moment it’s not clear how to structure it.

--- server.go
+++ server.go
@@ -173,3 +173,44 @@ func (handler *eventBusRequestHandler) prepareAuthContext(w http.ResponseWriter,
 	}
 	return
 }
+
+// routing and sending events
+
+func routeEvent(event *Event) {
+	clientAccessToken := event.Context["identifier"]
+
+	// If client access token is present, then send to client
+	if clientAccessToken != "" {
+		notifyClient(clientAccessToken, event)
+	}
+
+	// Broadcast the event to services
+	notifyServices(event)
+}
+
+func notify(event *Event) {
+	eventJSON, err := json.Marshal(event)
+	if err != nil {
+		log.Printf("Error marshaling event JSON: %s", err)
+		return
+	}
+	websocketHub.send(eventJSON)
+}
+
+func notifyClient(clientAccessToken string, event *Event) {
+	eventJSON, err := json.Marshal(event)
+	if err != nil {
+		log.Printf("Error marshaling event JSON: %s", err)
+		return
+	}
+	websocketHub.sendToClient(clientAccessToken, eventJSON)
+}
+
+func notifyServices(event *Event) {
+	eventJSON, err := json.Marshal(event)
+	if err != nil {
+		log.Printf("Error marshaling event JSON: %s", err)
+		return
+	}
+	websocketHub.sendToServices(eventJSON)
+}
